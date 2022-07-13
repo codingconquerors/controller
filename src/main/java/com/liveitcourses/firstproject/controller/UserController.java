0e7f29810e2a472f1f467f3f6362cd182985137a
khushwinder <chahalkhushwinder@gmail.com>
2022-07-12 21:43:49 -0400

    validation lecture

--- src/main/java/com/liveitcourses/firstproject/controller/UserController.java
+++ src/main/java/com/liveitcourses/firstproject/controller/UserController.java
@@ -6,10 +6,21 @@ import com.liveitcourses.firstproject.dto.User;
 import com.liveitcourses.firstproject.exception.UserNotFoundException;
 import com.liveitcourses.firstproject.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @Autowired
     private UserDaoService service;
@@ -24,10 +35,23 @@ public class UserController {
         return service.findOne(id);
     }
 
+    @GetMapping("/user/{id}")
+    public EntityModel<User> retrieveUserWithHateOS(@PathVariable int id) {
+
+        User user =  service.findOne(id);
+        EntityModel<User> model = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        model.add(linkToUsers.withRel("all-users"));
+
+        return model;
+    }
+
     //   input - details of user
     // output - CREATED & Return the created URI
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public void createUser(@Valid @RequestBody User user){
         User savedUser = service.save(user);
     }
 
@@ -37,6 +61,12 @@ public class UserController {
 
         if(user==null)
             throw new UserNotFoundException("id-"+ id);
+    }
+
+    @GetMapping(path = "/i18l")
+    public String internationalization() {
+        return messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 
 }
